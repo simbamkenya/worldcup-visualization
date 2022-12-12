fbebf5cb0b2bf3c5b7721ab1b92d46548bafd525
simbankenya <pharesmuruthi@gmail.com>
2022-12-12 07:46:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-gray-200 min-h-screen pb-24'>
+      <header className='w-full h-28 bg-red-100'>
+
+      </header>
+
+      <aside className='min-h-screen bg-green-100 w-36'>
+
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
